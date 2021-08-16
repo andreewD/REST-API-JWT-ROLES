115c28ff7e4a1c15eb81789db717dec5fdf81197
andreewaD <51123966+andreewD@users.noreply.github.com>
2021-08-15 23:07:16 -0500

    final commit

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,8 @@ import Router from "express";
 const router=Router()
 
 import * as authCtrl from "../controllers/auth.controller";
-
+import {verifySignup} from '../middlewares'
 router.post('/signin',authCtrl.signIn)
-router.post('/signup',authCtrl.signUp)
+router.post('/signup',[verifySignup.checkDuplicateUsernameOrEmail,verifySignup.checkRolesExisted],authCtrl.signUp)
 
 export default router;
